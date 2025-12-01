6ccc2f18cb76a26a03375c0ef8a433727747ad93
Pratomo Bowo Leksono <pratomobowo@gmail.com>
2025-12-01 18:25:54 +0700

    fix: Suppress hydration warning caused by browser extensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="id">
       <body
         className={`${fredoka.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
